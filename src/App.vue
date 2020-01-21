0553cedb0a12f7d0d7fb55fd1c5dd8d31026ddfb
ming <xiemingzhi@gmail.com>
2020-01-21 11:05:24 +0800

    Add v-tabs after v-content

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,29 @@
     <v-content>
       <router-view/>
     </v-content>
+
+    <v-tabs
+      v-model="tab"
+      background-color="deep-purple accent-4"
+      centered
+      dark
+      icons-and-text
+    >
+      <v-tabs-slider></v-tabs-slider>
+      <v-tab href="#tab-1">
+        Recents
+        <v-icon>mdi-phone</v-icon>
+      </v-tab>
+      <v-tab href="#tab-2">
+        Favorites
+        <v-icon>mdi-heart</v-icon>
+      </v-tab>
+      <v-tab href="#tab-3">
+        Nearby
+        <v-icon>mdi-account-box</v-icon>
+      </v-tab>
+    </v-tabs>
+
   </v-app>
 </template>
 
